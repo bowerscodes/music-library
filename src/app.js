127ca58ca9868bd6e0782641ff936ec94dd4741e
James Bowers <jamesbowers123@gmail.com>
2022-05-03 17:22:14 +0100

    setup database environment

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require ('express');
+
+const app = express();
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.status(200).res.body("Hello, World!")
+});
+
+
+
+
+
+module.exports = app;
