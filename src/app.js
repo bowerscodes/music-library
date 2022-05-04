91dcfac827a22f0ea678c48d127a1321bbc3f5ea
James Bowers <jamesbowers123@gmail.com>
2022-05-04 09:29:37 +0100

    create artist controller

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 const express = require ('express');
+const artistRouter = require('./routes/artist');
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/artist', artistRouter);
+
+
+
 
 app.get('/', (req, res) => {
     res.status(200).res.body("Hello, World!")
 });
-
 
 
 
---
+++ src/controllers/artist.js
@@ -0,0 +1,10 @@
+// Controller Function to return 201 code
+exports.create = (req, res) => {
+    res.sendStatus(201);
+}
+
+
+
+
+// // Not needed when using exports.create
+// module.exports = artistController;
