48b5bee566040b2b3e39f55646f8af1e65ad0f3a
James Bowers <jamesbowers123@gmail.com>
2022-05-04 15:29:11 +0100

    add test to create artist

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,10 +1,26 @@
-// Controller Function to return 201 code
-exports.create = (req, res) => {
-    res.sendStatus(201);
-}
+const getDb = require('../services/db');
+
+// // Controller Function to return 201 code
+// exports.create = (req, res) => {
+//     res.sendStatus(201);
+// }
+
+exports.create = async (req, res) => {
+    const db = await getDb();
+    const { name, genre } = req.body;
+
+    try {
+        await db.query(`INSERT INTO Artist (name, genre) VALUES ('${name}', '${genre}')`);
+        
+        res.sendStatus(201);
+    }  catch (err) {
+        res.sendStatus(500).json(err);
+    }
+
+    db.close();
+};
 
 
 
-
-// // Not needed when using exports.create
+// // Not needed when using exports.create:
 // module.exports = artistController;
